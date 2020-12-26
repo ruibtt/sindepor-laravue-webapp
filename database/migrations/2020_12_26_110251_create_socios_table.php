97eb56cd7cda0aeea1e34dc6c0f072d81e6c8acf
ruibtt <bettencourt.rui@gmail.com>
2020-12-26 21:04:46 +0000

    Migração para criar a tabela dos Sócios.

---
+++ database/migrations/2020_12_26_110251_create_socios_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSociosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('socios', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->integer('num_socio')->unique();
+            $table->string('regiao');
+            $table->string('local_trabalho');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('socios');
+    }
+}
